5a8d9d55674e419d004b2a1fe02f2b7d402a1012
BBonomi <bbaratari@gmail.com>
2020-09-18 20:25:33 -0300

    Adicionando rotas no React native

---
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+import Header from '../../components/Header';
+
+const CreateRecord = () => {
+    return (
+        <>
+        <Header/>
+        <Text style={styles.text}> Hello create-record!</Text>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: '#FFF',
+        fontSize:  50,
+        textAling: 'center',
+        marginTop: 100   
+    }
+  });
+  
+
+export default CreateRecord;
